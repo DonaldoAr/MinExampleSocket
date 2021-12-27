6b757f2ca3eefeab46b59e7fb87dca39395f3232
DonaldoAr <jefe.ceo@gmail.com>
2021-12-27 10:45:49 -0600

    Example show

--- public/js/socket-clien.js
+++ public/js/socket-clien.js
@@ -1,4 +1,24 @@
 console.log("Hola mundo");
+
+// REFERENCIAS DEL HTML
+// const lblOnline = document.getElementById("lblOnline");
+// const lblOffline = document.getElementById("lblOffline");
+    // TAMBIEN PODEMOS UTILIZAR querySelector
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 // AQUI TENEMOS QUE CONECTARNOS A LOS SOCKETS
-// socket de cliente
 const socket = io();
+// listers or observables que escuchan
+// ESTE LISTENER AYUDA A SABER SI ESTA CONECTADO AL SERVIDOR
+socket.on('connect',()=>{
+    console.log("Conectado");
+    
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+// EVENTO DE DESCONEXION CON EL SERVIDOR
+socket.on('disconnect',()=>{
+    console.log("Disconnect of server");
+});
